9d1431e0a5b232199a9d14c2339ede6adf35275e
Bryan-Abreu <djflights@gmail.com>
2023-05-18 18:42:51 -0700

    added graphCms packagejson, added eslint packagejson, made an api call on the header, added .map for the catagories, and added 2 files for the graphCms

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import Style from "../styles/Header.module.css"
+import Style from "../styles/Header.module.css";
+import { graphCms } from "../lib/graphCms";
 
 const Header = () => {
+  const [categoryLinks, setCategoryLinks] = useState([]);
+  useEffect(() => {
+    async function fetchData (){
+    const { categories } = await graphCms.request(`
+        query Assets {
+            categories {
+              color {
+                css
+              }
+              name
+            }
+          }
+        `);
+        setCategoryLinks(categories);
+    }
+    fetchData();
+  }, []);
   return (
-    <header className={  Style.header}>
+    <header className={Style.header}>
       <div className={Style.container}>
-        <Link className={Style.logo} href="/" >
-           86it.</Link>
-        <ul >
-          <li>
-            <Link className={Style.headerText} href="/">Category1</Link>
-          </li>
-          <li>
-            <Link href="/">Category2</Link>
-          </li>
+        <Link className={Style.logo} href="/">
+          86it.
+        </Link>
+        <ul>
+          {categoryLinks.map((link) => (
+            <li key={link.name}>
+              <Link href={`/${link.name}`}>
+                {link.name}
+                </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </header>
